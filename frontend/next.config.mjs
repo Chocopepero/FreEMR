f79aaa7546f440e7ab2403f8b8a0d31c89ca11e0
Samuel Kim <samkunkim@gmail.com>
2025-04-20 23:46:46 -0700

    Potential fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: process.env.NEXT_PUBLIC_APP_BACKEND_URL + '/api/:path*',
+            },
+        ];
+    },
+};
+
 export default nextConfig;
